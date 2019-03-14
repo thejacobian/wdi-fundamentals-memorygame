970148e0b590ffa9bce0b7b701aec19cfc6c1315
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-03-14 10:49:14 -0600

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,22 @@
 console.log('Up and running!');
 
-let cardOne = 'queen';
-let cardTwo = 'queen';
-let cardThree = 'king';
-let cardFour = 'king';
+let cards = ['queen', 'queen', 'king', 'king'];
+let cardsInPlay = [];
 
-console.log('User flipped: ' + cardOne);
-console.log('User flipped: ' + cardTwo);
+let cardOne = cards[2];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardsInPlay[0]);
+
+let cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+  if(cardsInPlay[0] === cardsInPlay[1]) {
+    alert('You found a match!');
+  } else {
+    alert('Sorry, try again.');
+  }
+}
+
+//alert('Hello, friends.');
